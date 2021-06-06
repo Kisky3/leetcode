5148cd745c5515a6cf8e068380b799805a337944
kisky3 <kisky3@126.com>
2021-06-06 23:47:24 +0900

    :+1: add some comment

--- blob/main/Binary-Search/852/solution2.js
+++ blob/main/Binary-Search/852/solution2.js
@@ -1,1 +1,10 @@
-// 852. 山脉数组的峰顶索引
+/**
+ * https://leetcode.com/problems/peak-index-in-a-mountain-array/
+ *
+ * 852. Peak Index in a Mountain Array
+ *
+ * Easy
+ *
+ * 48ms 98.70%
+ * 34.3mb 72.89%
+ */
