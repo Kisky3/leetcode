610e03831eb344729b932a7e284c46c07b8032a6
kisky3 <kisky3@126.com>
2021-06-08 21:55:58 +0900

    :+1: add some solution

--- Binary-Search/easy_744_solution.js
+++ Binary-Search/easy_744_solution.js
@@ -14,5 +14,24 @@
  */
 var nextGreatestLetter = function(letters, target) {
     // 如果数列里的最大的字母都比对象字母小的情况下，返回第二次循环的第一个字母
-    
+     if(letters[max - 1] <= target) {
+      return letters[0];
+   }
+
+  const last = binarySearch(letters, 0, max -1, target);
+  return letters[last]
 };
+
+function binarySearch(array, first, last, target) {
+  while (first < last) {
+      const mid = Math.floor(first + (last - first)/2)
+      if (target < array[mid]) {
+          last = mid
+      } else {
+          first = mid + 1
+      }
+  }
+
+  return last
+}
+
