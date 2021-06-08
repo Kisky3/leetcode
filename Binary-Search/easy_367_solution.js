36084ab9ed52b28dc95a5457a8e239ad5ce400f7
kisky3 <kisky3@126.com>
2021-06-08 22:00:09 +0900

    :+1: add 367 problem

---
+++ Binary-Search/easy_367_solution.js
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/valid-perfect-square/
+ *
+ * 367. Valid Perfect Square
+ *
+ * Easy
+ *
+ */
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    
+};
