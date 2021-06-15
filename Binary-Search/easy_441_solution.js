67ec169fd2376a34323feb0e43d0d88ee25b3a69
kisky3 <kisky3@126.com>
2021-06-15 23:35:54 +0900

    :shirt: eslintの修正

--- Binary-Search/easy_441_solution.js
+++ Binary-Search/easy_441_solution.js
@@ -17,7 +17,7 @@ var arrangeCoins = function(n) {
 
 function binarySearch(number) {
   let line = 1;
-  while ( number - ((1 + line) * line) / 2 > line) {
+  while (number - ((1 + line) * line) / 2 > line) {
     line += 1
   }
     return line;
