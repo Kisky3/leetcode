7afcbbdba96c3a144b15051870b0a94ee79aa7a9
kisky3 <kisky3@126.com>
2021-06-15 21:47:01 +0900

    add 411

--- Binary-Search/easy_441_solution.js
+++ Binary-Search/easy_441_solution.js
@@ -12,5 +12,13 @@
  * @return {number}
  */
 var arrangeCoins = function(n) {
-    
+  return binarySearch(n);
 };
+
+function binarySearch(number) {
+  let line = 1;
+  while ( number - ((1 + line) * line) / 2 > line) {
+    line += 1
+  }
+    return line;
+}
