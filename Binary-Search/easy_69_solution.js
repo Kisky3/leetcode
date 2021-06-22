86d0bc3aa92e9d19f8b8da98d553b9c4470d038b
kisky3 <kisky3@126.com>
2021-06-22 20:12:31 +0900

    add 69 problem

---
+++ Binary-Search/easy_69_solution.js
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.com/problems/sqrtx/
+ *
+ * 69. Sqrt(x)
+ *
+ * Easy
+ *
+ * Runtime: 144 ms, faster than 5.03% of JavaScript online submissions for Peak Index in a Mountain Array.
+ * Memory Usage: 39 MB, less than 93.74% of JavaScript online submissions for Peak Index in a Mountain Array.
+ */
+
+ /**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    
+};
