bcfe6a5335202ac1703bd411d74e68ab4e0d1441
kisky3 <kisky3@126.com>
2021-07-27 21:40:39 +0900

    add answer

--- Binary-Search/easy_875_solution.js
+++ Binary-Search/easy_875_solution.js
@@ -14,6 +14,25 @@
  * @param {number} h
  * @return {number}
  */
-var minEatingSpeed = function(piles, h) {
-    
+const minEatingSpeed = function(piles, h) {
+    let start = 1
+    let end = Math.max(...piles)
+    while(start < end) {
+      const mid = Math.floor(start + (end - start)/2);
+      let hour = computeHour(piles, mid)
+      if (hour > H) {
+        start = mid + 1
+      } else {
+        end = mid
+      }
+    }
+    return start
 };
+
+function computeHour (piles, per) {
+  let hours = 0
+  for (let p of piles) {
+    hours += Math.ceil(p / per)
+  }
+  return hours
+}
