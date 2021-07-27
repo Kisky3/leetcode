de0fa51487926856f345ab76180ff5d9f70a4000
kisky3 <kisky3@126.com>
2021-07-27 21:12:33 +0900

    :+1: create 875

---
+++ Binary-Search/easy_875_solution.js
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/problems/koko-eating-bananas/
+ *
+ * 875. Koko Eating Bananas
+ *
+ * Easy
+ *
+ * Runtime: 144 ms, faster than 5.03% of JavaScript online submissions for Peak Index in a Mountain Array.
+ * Memory Usage: 39 MB, less than 93.74% of JavaScript online submissions for Peak Index in a Mountain Array.
+ */
+
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    
+};
